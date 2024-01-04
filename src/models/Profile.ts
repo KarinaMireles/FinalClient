829904e9113911dc437a564dca126adc12824029
Karina <karinamls66@gmail.com>
2024-01-03 19:40:10 -0500

    passing state

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -3,4 +3,8 @@ export interface UserProfile {
   email: string;
   bio: string;
   age: number | null;
+  profilePhoto: string | null;
+  musicGenres: string[] | null;
+  location: string | null;
+  id: string;
 }
